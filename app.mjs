0a5ef1b458898e6cee7c9e1f0d03ded6991bd8da
bryanvengwa <bmvengwa@gmail.com>
2024-03-26 13:46:20 +0200

    successfully imported feed routes

--- app.mjs
+++ app.mjs
@@ -1,9 +1,18 @@
 import express from 'express';
+import feedRoutes from './routes/feed.mjs'
+
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+
+// GET /feed/post
+app.use('/feed', feedRoutes)
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`listening on ${PORT}`);
-});
+}); 
--- routes/feed.mjs
+++ routes/feed.mjs
@@ -1,5 +1,8 @@
 import express from 'express';
-import {getPost} from '../controllers/feed.mjs'
+import { getPost } from '../controllers/feed.mjs';
+
 const router = express.Router();
 
-router.get('/posts');
+router.get('/posts', getPost);
+
+export default router;
