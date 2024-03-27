76b6b1b1e9ef3b5e594e5040a88e5e77c879d697
bryanvengwa <bmvengwa@gmail.com>
2024-03-27 21:16:06 +0200

    updated dummy data

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Rest api comes with authentication using jwt tokens
+
+this is code that controls requests and resolves CORS errors
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST,PUT, DELETE,PATCH'
+  );
+  response.setHeader('Access-Control-Allow-Headers','Content-Type', 'Authorization');
+  next();
+});
+
+application of each access-control method should be revisited
--- controllers/feed.mjs
+++ controllers/feed.mjs
@@ -3,51 +3,61 @@ const dummyData = [
     id: 1,
     title: 'First Post',
     content: 'This is the content of the first post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 2,
     title: 'Second Post',
     content: 'This is the content of the second post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 3,
     title: 'Third Post',
     content: 'This is the content of the third post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 4,
     title: 'Fourth Post',
     content: 'This is the content of the fourth post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 5,
     title: 'Fifth Post',
     content: 'This is the content of the fifth post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 6,
     title: 'Sixth Post',
     content: 'This is the content of the sixth post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 7,
     title: 'Seventh Post',
     content: 'This is the content of the seventh post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 8,
     title: 'Eighth Post',
     content: 'This is the content of the eighth post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 9,
     title: 'Ninth Post',
     content: 'This is the content of the ninth post.',
+    imageUrl:'images/duck.jpg',
   },
   {
     id: 10,
     title: 'Tenth Post',
     content: 'This is the content of the tenth post.',
+    imageUrl:'images/duck.jpg',
   },
 ];
 
