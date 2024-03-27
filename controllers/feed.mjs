0ae7121ebb1574a8c76c4a3ed79593e2656712f0
bryanvengwa <bmvengwa@gmail.com>
2024-03-27 21:28:00 +0200

    created a creator property on the da=ummy data

--- controllers/feed.mjs
+++ controllers/feed.mjs
@@ -1,63 +1,113 @@
 const dummyData = [
   {
-    id: 1,
+    _id: 1,
     title: 'First Post',
     content: 'This is the content of the first post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 2,
+    _id: 2,
     title: 'Second Post',
     content: 'This is the content of the second post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 3,
+    _id: 3,
     title: 'Third Post',
     content: 'This is the content of the third post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 4,
+    _id: 4,
     title: 'Fourth Post',
     content: 'This is the content of the fourth post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 5,
+    _id: 5,
     title: 'Fifth Post',
     content: 'This is the content of the fifth post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 6,
+    _id: 6,
     title: 'Sixth Post',
     content: 'This is the content of the sixth post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 7,
+    _id: 7,
     title: 'Seventh Post',
     content: 'This is the content of the seventh post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 8,
+    _id: 8,
     title: 'Eighth Post',
     content: 'This is the content of the eighth post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 9,
+    _id: 9,
     title: 'Ninth Post',
     content: 'This is the content of the ninth post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
   {
-    id: 10,
+    i_d: 10,
     title: 'Tenth Post',
     content: 'This is the content of the tenth post.',
     imageUrl:'images/duck.jpg',
+    creator:{
+      name:'bryanvengwa',
+      
+    },
+    date: new Date()
   },
 ];
 
